9489cff52e49c06e6beba2f19434f4d80df11559
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-12 11:54:30 +0530

    data-types

--- 07-array.sh
+++ 07-array.sh
@@ -4,4 +4,8 @@ MOVIES=("court" "little-hearts" "ghati" "narasimha")
 
 echo "first movie is: ${MOVIES[0]}"
 echo "Third movie is : ${MOVIES[2]}"
+echo "fourth movie is: ${MOVIES[4]}"
+
+echo "Print all movies:${MOVIES[@]}"
+
 
