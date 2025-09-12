0367f88969181dae5e73c8d0bce3e039b7d4c810
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-12 07:54:09 +0530

    conversation started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "Trump:: hi musk,how are you doing ?"
+echo "musk:: hi trump, i am doing good "
+echo "trump:: whats'up..where you going"
+echo "musk:: I am going to space..will you join..?"
+echo "trump:: No thanks..i will come once you come back"
