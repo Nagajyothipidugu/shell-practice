83363336c544ee4764bb23940a3cc01aa8a9ac09
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-12 12:01:23 +0530

    data-types

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Trump  # there should be no space before and after =
 PERSON2=Musk
 
-echo "$PERSON1:: hi $PERSON2,how are you doing ?"
+echo "PERSON1:: hi $PERSON2,how are you doing ?"
 echo "$PERSON2:: hi $PERSON1, i am doing good "
 echo "$PERSON1:: whats'up..where you going"
 echo "$PERSON2:: I am going to space..will you join..?"
