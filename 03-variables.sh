43fe986d0d753f19d93c3d00ef64df3465fca90c
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-12 08:05:16 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: hi $PERSON2,how are you doing ?"
+echo "$PERSON2:: hi $PERSON1, i am doing good "
+echo "$PERSON1:: whats'up..where you going"
+echo "$PERSON2:: I am going to space..will you join..?"
+echo "$PERSON1:: No thanks..i will come once you come back"
