7ce7a7b2d9c23077159e9a43a687277fdf82ea2f
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-13 11:28:38 +0530

    special-var

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -2,3 +2,11 @@
 
 
 echo "All variables passed to the script :: $@"
+echo "Number of variables passed:: $#"
+echo "Name of the current-script :: $0" 
+echo "Present working directory :: $PWD" 
+echo "user running this script :: $USER"
+echo "Home directory of the user :: $HOME"
+echo "PID of the script :: $$"
+sleep 10 & 
+echo "PID of the last command in the background :: $!" 
